5b394af7d95171aca4c42a23bfa8b62a0569ad7f
cjoshua <cjoshua@chalmers.se>
2024-03-14 15:34:48 +0100

    Create day1_ project_brand_name_generator .py

---
+++ day1/day1_ project_brand_name_generator .py
@@ -0,0 +1,7 @@
+print("Hello this is band-name-generator \n")
+print("What city did you grow up in?")
+city = input()
+print("What is the name of your pet?")
+pet = input()
+combined = city + " " + pet 
+print("Your band name is: " + combined)
